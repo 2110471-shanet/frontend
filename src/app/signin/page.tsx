65bd1cb105c277ca117c2a4076e4053da068f735
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-04-10 22:01:45 +0700

    add ui signup/in page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+import AuthPane from "@/components/authpage/AuthPane";
+
+export default function SignIn() {
+    return (
+        <div className="bg-slate-50 min-h-screen flex justify-center items-center flex-nowrap px-4">
+            <AuthPane isSignUp={false} />
+        </div>
+    );
+}
