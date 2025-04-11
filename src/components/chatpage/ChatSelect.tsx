f71b5533091ed6433ffc60b3184750081c480b0e
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-04-11 19:12:06 +0700

    unfinish chatroom

---
+++ src/components/chatpage/ChatSelect.tsx
@@ -0,0 +1,12 @@
+export default function ChatSelect() {
+    return (
+        <div className="flex-nowrap h-full hidden lg:flex">
+            <div className="w-68 xl:w-80 bg-pink-300 shrink-0">
+
+            </div>
+            <div className="w-68 xl:w-80 bg-yellow-300 shrink-0">
+
+            </div>
+        </div>
+    );
+}
