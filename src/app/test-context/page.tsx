6287fd00d386250e31b20dce791047d0c801d578
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-04-13 11:33:22 +0700

    add test show member

--- src/app/test-context/page.tsx
+++ src/app/test-context/page.tsx
@@ -1,9 +1,11 @@
+import TestGroupMemberContext from "@/components/testpage/TestGroupMemberContext";
 import TestUsernameContext from "@/components/testpage/TestUsernameContext";
 
 export default function TestContext() {
     return (
-        <div className="h-screen flex justify-center items-center bg-slate-50">
+        <div className="h-screen flex flex-col gap-4 justify-center items-center bg-slate-50">
             <TestUsernameContext />
+            <TestGroupMemberContext />
         </div>
     );
 }
---
+++ src/components/testpage/TestGroupMemberContext.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { useContext, useEffect, useState } from "react";
+import { useGroupMember } from "@/components/provider/GroupMemberProvider";
+
+export default function TestGroupMemberContext() {
+    const {isShowingMember, members, groupName, setIsShowingMember, setMembers, setGroupName} = useGroupMember();
+
+    const [mockGroups, setMockGroups] = useState([
+        {
+            groupName: "NinjaKayak",
+            members: [
+                "Robertson",
+                "Lorly",
+                "Bendaway",
+                "Hillery",
+            ],
+        },
+        {
+            groupName: "Lanaconda",
+            members: [
+                "ImSoLonely",
+                "SideKick",
+            ],
+        },
+        {
+            groupName: "WeAreBobbyWeAreBobbyWeAreBobby",
+            members: [
+                "Bobby1",
+                "Bobby2",
+                "Bobby3",
+            ]
+        }
+    ]);
+
+    const [activeGroupIndex, setActiveGroupIndex] = useState<number | null>(null);
+
+    useEffect(() => {
+        if (activeGroupIndex !== null && mockGroups[activeGroupIndex]) {
+            setGroupName(mockGroups[activeGroupIndex].groupName);
+            setMembers(mockGroups[activeGroupIndex].members);
+        }
+    }, [activeGroupIndex, mockGroups]);
+
+    return (
+        <div className="flex gap-2">
+            <button className="px-4 py-2 bg-green-600 rounded-md text-white hover:cursor-pointer hover:bg-green-700 active:bg-green-800 duration-100" onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsShowingMember(!isShowingMember);
+                setActiveGroupIndex(0);
+            }}>
+                Show Members Group {mockGroups[0].groupName}
+            </button>
+            <button className="px-4 py-2 bg-green-600 rounded-md text-white hover:cursor-pointer hover:bg-green-700 active:bg-green-800 duration-100" onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsShowingMember(!isShowingMember);
+                setActiveGroupIndex(1);
+            }}>
+                Show Members Group {mockGroups[1].groupName}
+            </button>
+            <button className="px-4 py-2 bg-green-600 rounded-md text-white hover:cursor-pointer hover:bg-green-700 active:bg-green-800 duration-100" onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsShowingMember(!isShowingMember);
+                setActiveGroupIndex(2);
+            }}>
+                Show Members Group {mockGroups[2].groupName}
+            </button>
+        </div>
+    );
+}
