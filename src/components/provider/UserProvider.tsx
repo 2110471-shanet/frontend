ad500b51f1f5ac9674de042ec37359a2eede9764
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-04-17 14:10:07 +0700

    add pathname dependency

--- src/components/provider/UserProvider.tsx
+++ src/components/provider/UserProvider.tsx
@@ -44,7 +44,7 @@ export default function UserProvider({
             setIsLoading(false);
         };
         testLoadUsername();
-    }, [])
+    }, [pathname])
 
     return (
         <UserContext value={contextValue}>
