eb83eaae0e84695c935473e5b7636802f4c45416
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2025-04-21 11:57:01 +0700

    change to dynamic viewport

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import AuthPane from "@/components/authpage/AuthPane";
 
 export default function SignUp() {
     return (
-        <div className="bg-slate-50 min-h-screen flex justify-center items-center flex-nowrap px-4">
+        <div className="bg-slate-50 min-h-[100dvh] flex justify-center items-center flex-nowrap px-4">
             <AuthPane isSignUp={true} />
         </div>
     );
